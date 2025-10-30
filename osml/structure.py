96946242d46db3ec57de489e58d2bc8fa75dff1c
Shinypot8o <shinypot8o@slambodia.com>
2025-10-30 07:42:00 -0600

    bit more parser work

--- osml/structure.py
+++ osml/structure.py
@@ -1,17 +1,15 @@
 class Attribute:
-	def __init__(self, type, value, default, other):
+	def __init__(self, type, default, other):
 		self.type = type
 		self.default = default
 		self.other = other
 
 class Object:
 	def __init__(self):
-		self.attributes = []
+		self.attributes = {}
 
-	def _add_attribute(self, name, default, other):
-		pass
-		
-
+	def _add_attribute(self, name, attr: Attribute):
+		self.attributes.update(name, attr)
 
 class ObjectInstance:
 	def __init__(self):
