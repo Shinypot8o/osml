81e7104ac84a3e73f44908906cd8e5f170bae93a
Trevor Abercrombie <trevor.abercrombie@gmail.com>
2025-10-29 21:52:18 -0600

    rethink, make md, start parser

---
+++ osml.py
@@ -0,0 +1,23 @@
+class Attribute:
+	def __init__(self, type, value, default, other):
+		self.type = type
+		self.default = default
+		self.other = other
+
+class Object:
+	def __init__(self):
+		self.attributes = []
+
+	def _add_attribute(self, name, default, other):
+		pass
+		
+
+
+class ObjectInstance:
+	def __init__(self):
+		self.type
+
+
+class Scene:
+	def __init__(self):
+		pass
